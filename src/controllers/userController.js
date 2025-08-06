2430a424629e9d0fefe91aa0f40e2a09363d02cf
Sanjay kumar <sanjayofcl8757@gmail.com>
2025-08-06 22:43:03 +0530

    userController-tryCatch

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,19 +1,19 @@
 const Users = require("../models/userModel");
 const userEntry = async (req, res) => {
-    try {
-         const { userName, password } = req.body;
-  const newUser = new Users({
-    userName,
-    password,
-  });
+  try {
+    const { userName, password } = req.body;
+    const newUser = new Users({
+      userName,
+      password,
+    });
 
-  await newUser.save();
-  res.json({ message: "User Created succesfully." });
-  res.status(201).send('success');
-    } catch (error) {
-        
-    }
- 
+    await newUser.save();
+    res.json({ message: "User Created succesfully." });
+    res.status(201).send("success");
+  } catch (error) {
+    res.json({message:'Server Error'+error});
+    res.status(500).send('failed');
+  }
 };
 
 module.exports = userEntry;
